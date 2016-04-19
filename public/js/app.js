859fbe7d566961b8958429b5c4a7d323bc74faff
Hugo A Guerra <guerrahugoa@gmail.com>
2016-04-19 09:49:35 -0700

    initialized gulpfile and webpack functionality

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+require(__dirname + '/../../bower_components/angular/angular.js');
+require(__dirname + '/../css/normalize.css');
+require(__dirname + '/../css/main.css');
+require(__dirname + '/../css/animate.css');
+// require(__dirname + '/profile/profile-module.js');
+// require(__dirname + '/profile/profile.js');
+// require(__dirname + '/repos/repos-module.js');
+// require(__dirname + '/repos/repos.js');
+
+(function() {
+  angular.module('directives-app', []);
+})();
